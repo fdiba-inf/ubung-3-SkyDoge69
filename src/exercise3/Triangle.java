7327bba705ab6041c4afbe6bae5ad455a64471ac
SkyDoge69 <luc2002penta@gmail.com>
2021-10-21 13:13:55 +0300

    Last task

---
+++ src/exercise3/Triangle.java
@@ -0,0 +1,34 @@
+package exercise3;
+
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a, b, c;
+        String res;
+        do {
+            a = input.nextDouble();
+            b = input.nextDouble();
+            c = input.nextDouble();
+
+        } while (a <= 0 || b <= 0 || c <= 0);
+
+
+        if (a == b && a == c && b == c) {
+            res = "equilateral";
+        } else if (a == b || b == c || c == a) {
+            res = "isosceles";
+        } else {
+            res = "scalene";
+        }
+
+        double p = (a + b + c)/2;
+        double area = sqrt(p * (p - a) * (p - b) * (p -c ));
+        System.out.println("Area: " + area);
+        System.out.println("Triangle:" + res);
+
+    }
+}
